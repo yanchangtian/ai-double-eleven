233f4aa2686868f624cdd4eafc1a6f0340790ebf
635606350@qq.com <yanct342516>
2021-06-15 21:54:13 +0800

    yct 1

--- src/main/java/com/yan/study/Application.java
+++ src/main/java/com/yan/study/Application.java
@@ -84,6 +84,8 @@ public class Application {
 		Node n3 = new Node(3);
 		Node n4 = new Node(4);
 		Node n5 = new Node(5);
+		Node n6 = new Node(6);
+
 		n1.next = n2;
 		n2.next = n3;
 		n3.next = n4;
